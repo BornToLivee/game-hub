26360f8424240319de976610397057e4c06b39bc
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-07-08 18:26:01 +0300

    create templates directory & create basic index.html start page & create index view

--- game/views.py
+++ game/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from game.models import Player, Game, Publisher, Genre
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    """View function for the home page of the site."""
+
+    num_players = Player.objects.count()
+    num_games = Game.objects.count()
+    num_publishers = Publisher.objects.count()
+    num_genres = Genre.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_players": num_players,
+        "num_games": num_games,
+        "num_publishers": num_publishers,
+        "num_genres": num_genres,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "game/index.html", context=context)
