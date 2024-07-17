c2553e06590859e08f9945afa50102fdc45c1145
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-07-17 17:08:22 +0300

    create publisher create, update, delete buttons and pages & create game_confirm_delete page with game delete button

--- game/urls.py
+++ game/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 
 from game.views import index, GameListView, GenreListView, PublisherListView, register, personal_page, \
     update_wishlist_status, update_completed_status, game_detail, GameCreateView, GameUpdateView, GenreDetailView, \
-    PublisherDetailView, about, RandomGameView, player_update, GenreCreateView, GenresUpdateView, GenreDeleteView
+    PublisherDetailView, about, RandomGameView, player_update, GenreCreateView, GenresUpdateView, GenreDeleteView, \
+    PublisherCreateView, PublisherUpdateView, PublisherDeleteView, GameDeleteView
 from game_hub import settings
 
 urlpatterns = [
@@ -12,6 +13,7 @@ urlpatterns = [
     path("games/<int:pk>/", game_detail, name="game-detail"),
     path("games/create/", GameCreateView.as_view(), name="game-create"),
     path("games/<int:pk>/update/",  GameUpdateView.as_view(), name="game-update"),
+    path("games/<int:pk>/delete/", GameDeleteView.as_view(), name="game-delete"),
     path("genres/", GenreListView.as_view(), name="genre-list"),
     path("ganres/<int:pk>/", GenreDetailView.as_view(), name="genre-detail"),
     path("genres/create/", GenreCreateView.as_view(), name="genre-create"),
@@ -19,6 +21,9 @@ urlpatterns = [
     path("genres/<int:pk>/delete/", GenreDeleteView.as_view(), name="genre-delete"),
     path("publishers/", PublisherListView.as_view(), name="publisher-list"),
     path("publishers/<int:pk>/", PublisherDetailView.as_view(), name="publisher-detail"),
+    path("publishers/create/", PublisherCreateView.as_view(), name="publisher-create"),
+    path("publishers/<int:pk>/update/", PublisherUpdateView.as_view(), name="publisher-update"),
+    path("publishers/<int:pk>/delete/", PublisherDeleteView.as_view(), name="publisher-delete"),
     path("register/", register, name="register"),
     path('update_wishlist_status/<int:game_id>/', update_wishlist_status, name='update-wishlist-status'),
     path('update_completed_status/<int:game_id>/', update_completed_status, name='update-completed-status'),
