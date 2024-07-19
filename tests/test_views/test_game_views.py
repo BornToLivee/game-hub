272338b487cb4cc4f25c8117e5c3af2ba733249c
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-07-19 03:01:48 +0300

    add test for player views and for game views

---
+++ tests/test_views/test_game_views.py
@@ -0,0 +1,158 @@
+from _decimal import Decimal
+from django.contrib.auth import get_user_model
+from django.core.files.base import ContentFile
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.http import HttpRequest
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from game.models import Game, Genre, Platform, Publisher, Player, Rating
+from game.views import GameListView, GameCreateView, GameDeleteView, GameDetailView
+from django.test import RequestFactory
+
+
+class GameListViewTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.genre = Genre.objects.create(name="Action", description="Action games")
+        self.platform = Platform.objects.create(name="PC")
+        self.publisher = Publisher.objects.create(name="Epic Games", description="Epic Games Publisher", country="USA", capitalization=Decimal("10.00"))
+        for i in range(10):
+            game = Game.objects.create(
+                title=f"Test Game {i + 1}",
+                description=f"Description for Test Game {i + 1}",
+                release_year=2023,
+                genre=self.genre,
+                publisher=self.publisher,
+                link=f"https://www.testgame{i + 1}.com",
+                image=ContentFile(f"image{i + 1}.jpg", name=f"test_image{i + 1}.jpg")
+            )
+            game.save()
+
+    def test_retrieve_games_with_pagination(self):
+        response = self.client.get(reverse('game:game-list') + '?page=2')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('page_obj', response.context)
+        page_obj = response.context['page_obj']
+        self.assertEqual(len(page_obj.object_list), 4)
+        self.assertEqual(page_obj.number, 2)
+
+    def test_filter_games_by_title(self):
+        Game.objects.create(
+            title="Test Game",
+            description="Description for Test Game",
+            release_year=2023,
+            genre=self.genre,
+            publisher=self.publisher,
+            link="https://www.testgame.com",
+            image=ContentFile("test_image.jpg", name="test_image.jpg")
+        )
+        response = self.client.get(reverse('game:game-list') + '?title=Test Game')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('object_list', response.context)
+        self.assertEqual(len(response.context['object_list']), 6)
+        self.assertEqual(response.context['object_list'][0].title, "Test Game")
+
+    def test_display_all_genres(self):
+        self.genre1 = Genre.objects.create(name="Horror", description="Horror games")
+        self.genre2 = Genre.objects.create(name="Adventure", description="Adventure games")
+        self.genre3 = Genre.objects.create(name="Strategy", description="Strategy games")
+        response = self.client.get(reverse('game:game-list'))
+
+        self.assertIn('genres', response.context)
+
+        genres_in_context = response.context['genres']
+
+        self.assertIn(self.genre1, genres_in_context)
+        self.assertIn(self.genre2, genres_in_context)
+        self.assertIn(self.genre3, genres_in_context)
+
+
+class GameDeleteViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = get_user_model().objects.create_user(username='testuser', password='12345')
+        self.genre = Genre.objects.create(name="Action", description="Action games")
+        self.platform = Platform.objects.create(name="PC")
+        self.publisher = Publisher.objects.create(
+            name="Epic Games",
+            description="Epic Games Publisher",
+            country="USA",
+            capitalization=Decimal("10.00")
+        )
+        self.game = Game.objects.create(
+            title="Test Game",
+            description="Description for Test Game",
+            release_year=2023,
+            genre=self.genre,
+            publisher=self.publisher,
+            link="https://www.testgame.com",
+            image=SimpleUploadedFile("test_image.jpg", b"file_content", content_type="image/jpeg")
+        )
+        self.game.platform.set([self.platform])
+        self.game.save()
+
+    def test_successful_deletion_logged_in_user(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.post(reverse('game:game-delete', kwargs={'pk': self.game.pk}))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Game.objects.filter(pk=self.game.pk).exists())
+        self.assertRedirects(response, reverse('game:game-list'))
+
+    def test_confirmation_prompt_displayed(self):
+        self.client.login(username='testuser', password='12345')
+        url = reverse("game:game-delete", kwargs={"pk": self.game.pk})
+        response = self.client.get(url)
+        self.assertContains(response, "Are you sure you want to delete")
+
+
+class GameDetailViewTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.client = Client()
+        self.user = get_user_model().objects.create_user(username='testuser', password='12345', email="qwe@qwe.com")
+        self.genre = Genre.objects.create(name="Action", description="Action games")
+        self.platform = Platform.objects.create(name="PC")
+        self.publisher = Publisher.objects.create(
+            name="Epic Games",
+            description="Epic Games Publisher",
+            country="USA",
+            capitalization=Decimal("10.00")
+        )
+        self.game = Game.objects.create(
+            title="Test Game",
+            description="Description for Test Game",
+            release_year=2023,
+            genre=self.genre,
+            publisher=self.publisher,
+            link="https://www.testgame.com",
+            image=SimpleUploadedFile("test_image.jpg", b"file_content", content_type="image/jpeg")
+        )
+        self.game.platform.set([self.platform])
+        self.game.save()
+
+    def test_renders_game_detail_for_authenticated_users(self):
+        self.client.login(username='testuser', password='12345')
+
+        url = reverse('game:game-detail', kwargs={'pk': self.game.pk})
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.game.title)
+        self.assertContains(response, self.game.description)
+        self.assertContains(response, self.game.release_year)
+        self.assertContains(response, self.game.link)
+        self.assertContains(response, self.game.image.url)
+        self.assertContains(response, "Average players rating")
+
+    def test_calculate_unique_user_votes(self):
+        player1 = Player.objects.create(username="player1", password="<PASSWORD>")
+        player2 = Player.objects.create(username="player2", password="<PASSWORD>")
+        Rating.objects.create(player=player1, game=self.game, score=8)
+        Rating.objects.create(player=player2, game=self.game, score=6)
+
+        self.client.force_login(player1)
+        response = self.client.get(reverse("game:game-detail", kwargs={"pk": self.game.pk}))
+
+        self.assertEqual(response.context["user_votes_count"], 2)
---
+++ tests/test_views/test_player_views.py
@@ -0,0 +1,189 @@
+from unittest.mock import MagicMock
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from django.test import RequestFactory
+from game.models import Player, Genre, Platform, Publisher
+from game.views import PlayerUpdateView, PersonalPageView
+
+
+class PlayerUpdateViewTestCase(TestCase):
+    def test_registration_form_renders_correctly(self):
+        self.client.login(username="testuser", password='testpass')
+        response = self.client.get(reverse('game:register'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/register.html')
+
+    def test_user_can_successfully_submit_registration_form(self):
+        client = Client()
+        response = client.post(reverse('game:register'), {
+            'username': 'test_user_1',
+            'password1': 'testpassword123tttttyy',
+            'password2': 'testpassword123tttttyy'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('game:personal-page'))
+
+    def test_form_submission_with_already_taken_username(self):
+        Player.objects.create_user(username="test_user", password="test_password")
+        form_data = {
+            "username": "test_user",
+            "email": "test@example.com",
+            "date_of_birth": "2000-01-01",
+            "first_name": "Test",
+            "last_name": "User",
+            "password1": "test_password3124",
+            "password2": "test_password3124"
+        }
+
+        response = self.client.post(reverse("game:register"), form_data)
+        self.assertFalse(response.context["form"].is_valid())
+
+
+class PlayerUpdateViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = Player.objects.create_user(
+            username='testuser1',
+            password='12345',
+            email='testuser@example.com'
+        )
+        self.client.login(username='testuser1', password='12345')
+
+    def test_successful_update(self):
+        response = self.client.post(reverse("game:player-update"), {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'date_of_birth': '1990-01-01'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.first_name, 'John')
+        self.assertEqual(self.user.last_name, 'Doe')
+        self.assertEqual(str(self.user.date_of_birth), '1990-01-01')
+
+    def test_redirects_on_success(self):
+        factory = RequestFactory()
+        request = factory.post('/player/update/', {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'date_of_birth': '1990-01-01'
+        })
+        request.user = Player.objects.create_user(username='testuser', password='12345')
+
+        view = PlayerUpdateView()
+        view.setup(request)
+        response = view.post(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/personal_page/')
+
+    def test_player_update_view_with_current_user_data(self):
+        client = self.client
+        player = get_user_model().objects.create_user(username='testuser', password='12345')
+        client.login(username='testuser', password='12345')
+        response = client.get(reverse('game:player-update'))
+        self.assertContains(response, player.username)
+
+    def test_correct_template_rendering(self):
+        response = self.client.get(reverse('game:player-update'))
+        self.assertTemplateUsed(response, 'game/player_update.html')
+
+
+class PersonalPageViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = get_user_model().objects.create_user(username='test_user', password='12345')
+        self.client.login(username='test_user', password='12345')
+
+        self.genre = Genre.objects.create(name='Test Genre', description='Test Genre')
+        self.platform = Platform.objects.create(name='Test Platform')
+        self.publisher = Publisher.objects.create(name='Test Publisher', description='Test Publisher', country='Ukraine', capitalization=0.02)
+
+    def test_user_with_wishlist_and_completed_games_sees_paginated_lists(self):
+        user = User(username='testuser')
+        user.wishlist_games = MagicMock()
+        user.completed_games = MagicMock()
+        user.wishlist_games.all.return_value = ['game1', 'game2', 'game3', 'game4', 'game5', 'game6']
+        user.completed_games.all.return_value = ['game1', 'game2', 'game3', 'game4', 'game5', 'game6']
+
+        request = RequestFactory().get('/personal_page')
+        request.user = user
+
+        view = PersonalPageView()
+        view.setup(request)
+
+        context = view.get_context_data()
+
+        self.assertIn('wishlist_games', context)
+        self.assertIn('completed_games', context)
+
+    def test_user_without_wishlist_or_completed_games_sees_empty_lists(self):
+        user = User(username='testuser')
+        user.wishlist_games = MagicMock()
+        user.completed_games = MagicMock()
+        user.wishlist_games.all.return_value = []
+        user.completed_games.all.return_value = []
+
+        request = RequestFactory().get('/personal_page')
+        request.user = user
+
+        view = PersonalPageView()
+        view.setup(request)
+
+        context = view.get_context_data()
+
+        self.assertEqual(len(context['wishlist_games']), 0)
+        self.assertEqual(len(context['completed_games']), 0)
+
+    def test_user_requests_non_existent_wishlist_page_number(self):
+        user = User(username='testuser')
+        user.wishlist_games = MagicMock()
+        user.completed_games = MagicMock()
+        user.wishlist_games.all.return_value = ['game1', 'game2', 'game3']
+        user.completed_games.all.return_value = ['game1', 'game2', 'game3']
+
+        request = RequestFactory().get('/personal_page', {'wishlist_page': 999})
+        request.user = user
+
+        view = PersonalPageView()
+        view.setup(request)
+
+        context = view.get_context_data()
+
+        self.assertEqual(context['wishlist_games'].number, 1)
+
+    def test_correct_template_rendered(self):
+        response = self.client.get(reverse('game:personal-page'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'game/personal_page.html')
+
+    def test_pagination_completed_games(self):
+        request = RequestFactory().get('/personal_page/?completed_page=2')
+        request.user = self.user
+
+        view = PersonalPageView()
+        view.request = request
+
+        context = view.get_context_data()
+
+        self.assertTrue('completed_games' in context)
+        self.assertTrue('is_completed_paginated' in context)
+
+    def test_pagination_wishlist_games(self):
+        request = RequestFactory().get('/personal_page/?wishlist_page=2')
+        request.user = self.user
+
+        view = PersonalPageView()
+        view.request = request
+
+        context = view.get_context_data()
+
+        self.assertTrue('wishlist_games' in context)
+        self.assertTrue('is_wishlist_paginated' in context)
+
+
