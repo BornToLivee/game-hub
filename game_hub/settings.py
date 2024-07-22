1aea3e9defff37015fcc698761b4e437b073b040
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-07-23 02:10:46 +0300

    remove insecure secret_key

--- game_hub/settings.py
+++ game_hub/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "=fenwsufx=cg6#n@+7np8clz@wpfelw#rxv=--7w-ew677_@8=")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
